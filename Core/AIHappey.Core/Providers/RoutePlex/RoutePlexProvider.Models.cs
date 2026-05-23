f5bafa5a06ab471274e9b46fbfd05e2ace73a390
Arthur Bleij <github@achappey.nl>
2026-05-23 21:20:25 +0200

    Add RoutePlex AI mode to provider model list

--- Core/AIHappey.Core/Providers/RoutePlex/RoutePlexProvider.Models.cs
+++ Core/AIHappey.Core/Providers/RoutePlex/RoutePlexProvider.Models.cs
@@ -83,6 +83,8 @@ public partial class RoutePlexProvider
                         models.Add(model);
                 }
 
+                models.AddRange(GetIdentifier().GetModels());
+
                 return models;
             },
             baseTtl: TimeSpan.FromHours(4),
