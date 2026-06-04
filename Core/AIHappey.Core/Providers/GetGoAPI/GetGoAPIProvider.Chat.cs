db7c0e7ab507b72406147b2a6aa8e9ca29fe36e9
Arthur Bleij <github@achappey.nl>
2026-06-04 09:25:23 +0200

    Implement GetGoAPI unified chat and responses streaming

--- Core/AIHappey.Core/Providers/GetGoAPI/GetGoAPIProvider.Chat.cs
+++ Core/AIHappey.Core/Providers/GetGoAPI/GetGoAPIProvider.Chat.cs
@@ -1,4 +1,5 @@
-using AIHappey.Core.AI;
+using AIHappey.Vercel.Mapping;
+using AIHappey.Vercel.Extensions;
 using System.Runtime.CompilerServices;
 using AIHappey.Vercel.Models;
 
@@ -9,10 +10,16 @@ public partial class GetGoAPIProvider
     public async IAsyncEnumerable<UIMessagePart> StreamAsync(ChatRequest chatRequest,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        ApplyAuthHeader();
+        var unifiedRequest = chatRequest.ToUnifiedRequest(GetIdentifier());
 
-        await foreach (var update in _client.CompletionsStreamAsync(chatRequest,
-            cancellationToken: cancellationToken))
-            yield return update;
+        await foreach (var part in this.StreamUnifiedAsync(
+            unifiedRequest,
+            cancellationToken))
+        {
+            foreach (var uiPart in part.Event.ToUIMessagePart(GetIdentifier()))
+            {
+                yield return uiPart;
+            }
+        }
     }
 }
