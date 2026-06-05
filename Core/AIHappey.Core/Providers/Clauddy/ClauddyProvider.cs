debdd99fe82431a1fd3c75fe03c222cd7de33149
Arthur Bleij <github@achappey.nl>
2026-06-05 14:35:43 +0200

    Disable SamplingAsync on Clauddy and Poolside providers

--- Core/AIHappey.Core/Providers/Clauddy/ClauddyProvider.cs
+++ Core/AIHappey.Core/Providers/Clauddy/ClauddyProvider.cs
@@ -56,12 +56,9 @@ public partial class ClauddyProvider : IModelProvider
 
     public string GetIdentifier() => nameof(Clauddy).ToLowerInvariant();
 
-       public async Task<CreateMessageResult> SamplingAsync(CreateMessageRequestParams chatRequest, CancellationToken cancellationToken = default)
+    public async Task<CreateMessageResult> SamplingAsync(CreateMessageRequestParams chatRequest, CancellationToken cancellationToken = default)
     {
-        var result = await ExecuteUnifiedAsync(chatRequest.ToUnifiedRequest(GetIdentifier()),
-           cancellationToken);
-
-        return result.ToSamplingResult();
+        throw new NotSupportedException();
     }
 
     public Task<TranscriptionResponse> TranscriptionRequest(TranscriptionRequest imageRequest, CancellationToken cancellationToken = default)
