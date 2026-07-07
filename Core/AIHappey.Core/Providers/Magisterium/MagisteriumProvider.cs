80acb72ffd6f1c0feef3ea8e8b826e691249c251
Arthur Bleij <github@achappey.nl>
2026-07-07 20:09:26 +0200

    Disable Magisterium sampling with NotSupportedException

--- Core/AIHappey.Core/Providers/Magisterium/MagisteriumProvider.cs
+++ Core/AIHappey.Core/Providers/Magisterium/MagisteriumProvider.cs
@@ -63,12 +63,9 @@ public partial class MagisteriumProvider : IModelProvider
 
     public string GetIdentifier() => nameof(Magisterium).ToLowerInvariant();
 
-    public async Task<CreateMessageResult> SamplingAsync(CreateMessageRequestParams chatRequest, CancellationToken cancellationToken = default)
+    public Task<CreateMessageResult> SamplingAsync(CreateMessageRequestParams chatRequest, CancellationToken cancellationToken = default)
     {
-        var result = await ExecuteUnifiedAsync(chatRequest.ToUnifiedRequest(GetIdentifier()),
-           cancellationToken);
-
-        return result.ToSamplingResult();
+       throw new NotSupportedException();
     }
 
     public Task<TranscriptionResponse> TranscriptionRequest(TranscriptionRequest imageRequest, CancellationToken cancellationToken = default)
