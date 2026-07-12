9259ece02ed0152e663b6663502af584fdf03595
Arthur Bleij <github@achappey.nl>
2026-07-12 21:11:43 +0200

    Make InfronProvider sampling throw NotSupportedException

--- Core/AIHappey.Core/Providers/Infron/InfronProvider.cs
+++ Core/AIHappey.Core/Providers/Infron/InfronProvider.cs
@@ -58,12 +58,9 @@ public partial class InfronProvider : IModelProvider
 
     public string GetIdentifier() => nameof(Infron).ToLowerInvariant();
 
-    public async Task<CreateMessageResult> SamplingAsync(CreateMessageRequestParams chatRequest, CancellationToken cancellationToken = default)
+    public Task<CreateMessageResult> SamplingAsync(CreateMessageRequestParams chatRequest, CancellationToken cancellationToken = default)
     {
-        var result = await ExecuteUnifiedAsync(chatRequest.ToUnifiedRequest(GetIdentifier()),
-           cancellationToken);
-
-        return result.ToSamplingResult();
+        throw new NotSupportedException();
     }
 
     public Task<TranscriptionResponse> TranscriptionRequest(TranscriptionRequest imageRequest, CancellationToken cancellationToken = default)
