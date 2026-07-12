f3cc6eb60419b0ee2cbee4b456c37c5f0592232f
Arthur Bleij <github@achappey.nl>
2026-07-12 18:16:37 +0200

    Classify orpheus models as speech in UI message extensions

--- Core/AIHappey.Core/Common/Extensions.cs
+++ Core/AIHappey.Core/Common/Extensions.cs
@@ -21,6 +21,7 @@ public static class UIMessagePartExtensions
 
         if (modelId.Contains("tts")
         || modelId.Contains("canopy")
+        || modelId.Contains("orpheus")
         || modelId.Contains("Kokoro", StringComparison.OrdinalIgnoreCase)
         || modelId.Contains("chatterbox"))
             return "speech";
