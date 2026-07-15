15bb80f74f245e11888a5aafabb7c7d75140204b
Arthur Bleij <github@achappey.nl>
2026-07-15 16:15:37 +0200

    Disable EUrouter SamplingAsync and throw NotSupportedException

--- Core/AIHappey.Core/Providers/EUrouter/EUrouterProvider.cs
+++ Core/AIHappey.Core/Providers/EUrouter/EUrouterProvider.cs
@@ -41,12 +41,9 @@ public partial class EUrouterProvider : IModelProvider
 
     public string GetIdentifier() => nameof(EUrouter).ToLowerInvariant();
 
-    public async Task<CreateMessageResult> SamplingAsync(CreateMessageRequestParams chatRequest, CancellationToken cancellationToken = default)
+    public Task<CreateMessageResult> SamplingAsync(CreateMessageRequestParams chatRequest, CancellationToken cancellationToken = default)
     {
-        var result = await this.ExecuteUnifiedAsync(chatRequest.ToUnifiedRequest(GetIdentifier()),
-            cancellationToken);
-
-        return result.ToSamplingResult();
+        throw new NotSupportedException();
     }
 
     public Task<TranscriptionResponse> TranscriptionRequest(TranscriptionRequest imageRequest, CancellationToken cancellationToken = default)
