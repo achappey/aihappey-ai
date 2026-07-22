11148b2ec6d9964ead1c0c0b70a49c9be920382b
Arthur Bleij <github@achappey.nl>
2026-07-22 18:20:53 +0200

    Filter GTranslate models by supported translation services

--- Core/AIHappey.Core/Providers/GTranslate/GTranslate.Models.cs
+++ Core/AIHappey.Core/Providers/GTranslate/GTranslate.Models.cs
@@ -6,65 +6,68 @@ namespace AIHappey.Core.Providers.GTranslate;
 
 public partial class GTranslateProvider
 {
-    public async Task<IEnumerable<Model>> ListModels(CancellationToken cancellationToken = default)
+    public Task<IEnumerable<Model>> ListModels(
+        CancellationToken cancellationToken = default)
     {
+        cancellationToken.ThrowIfCancellationRequested();
 
-        return await ListTranslationModelsAsync(cancellationToken);
+        var models = new List<Model>();
+
+        models.AddRange(CreateTranslationModels(
+            service: TranslationServices.Yandex,
+            modelPrefix: "yandex/translate-to/",
+            displayName: "Yandex"));
+
+        models.AddRange(CreateTranslationModels(
+            service: TranslationServices.Google,
+            modelPrefix: "google/v1/translate-to/",
+            displayName: "Google"));
+
+        models.AddRange(CreateTranslationModels(
+            service: TranslationServices.Google,
+            modelPrefix: "google/v2/translate-to/",
+            displayName: "Google V2"));
+
+        models.AddRange(CreateTranslationModels(
+            service: TranslationServices.Bing,
+            modelPrefix: "bing/translate-to/",
+            displayName: "Bing"));
+
+        models.AddRange(CreateTranslationModels(
+            service: TranslationServices.Microsoft,
+            modelPrefix: "microsoft/translate-to/",
+            displayName: "Microsoft"));
+
+        return Task.FromResult<IEnumerable<Model>>(models);
     }
 
-    private async Task<IEnumerable<Model>> ListTranslationModelsAsync(CancellationToken cancellationToken)
+    private IEnumerable<Model> CreateTranslationModels(
+      TranslationServices service,
+      string modelPrefix,
+      string displayName)
     {
-        var yandexModels = Language.LanguageDictionary.Select(a => new Model()
-        {
-            OwnedBy = nameof(GTranslate),
-            Type = "language",
-            Tags = [a.Key, "translate"],
-            Id = $"yandex/translate-to/{a.Key}".ToModelId(GetIdentifier()),
-            Name = $"Yandex Translate to {a.Value.Name}",
-            Description = a.Value.NativeName,
-        }).ToList();
+        return Language.LanguageDictionary
+            .Where(entry => entry.Value.IsServiceSupported(service))
+            .Select(entry =>
+            {
+                var languageCode = entry.Key;
+                var language = entry.Value;
 
-        var bingModels = Language.LanguageDictionary.Select(a => new Model()
-        {
-            OwnedBy = nameof(GTranslate),
-            Type = "language",
-            Tags = [a.Key, "translate"],
-            Id = $"bing/translate-to/{a.Key}".ToModelId(GetIdentifier()),
-            Name = $"Bing Translate to {a.Value.Name}",
-            Description = a.Value.NativeName,
-        }).ToList();
-
-        var microsoftModels = Language.LanguageDictionary.Select(a => new Model()
-        {
-            OwnedBy = nameof(GTranslate),
-            Type = "language",
-            Tags = [a.Key, "translate"],
-            Id = $"microsoft/translate-to/{a.Key}".ToModelId(GetIdentifier()),
-            Name = $"Microsoft Translate to {a.Value.Name}",
-            Description = a.Value.NativeName,
-        }).ToList();
-
-        var googleModels = Language.LanguageDictionary.Select(a => new Model()
-        {
-            OwnedBy = nameof(GTranslate),
-            Type = "language",
-            Tags = [a.Key, "translate"],
-            Id = $"google/v1/translate-to/{a.Key}".ToModelId(GetIdentifier()),
-            Name = $"Google Translate to {a.Value.Name}",
-            Description = a.Value.NativeName,
-        }).ToList();
-
-        var google2Models = Language.LanguageDictionary.Select(a => new Model()
-        {
-            OwnedBy = nameof(GTranslate),
-            Type = "language",
-            Tags = [a.Key, "translate"],
-            Id = $"google/v2/translate-to/{a.Key}".ToModelId(GetIdentifier()),
-            Name = $"Google Translate to {a.Value.Name}",
-            Description = a.Value.NativeName,
-        }).ToList();
-
-        return await Task.FromResult<IEnumerable<Model>>([.. yandexModels, .. googleModels, .. google2Models, .. bingModels, .. microsoftModels]);
+                return new Model
+                {
+                    OwnedBy = nameof(GTranslate),
+                    Type = "language",
+                    Tags =
+                    [
+                        languageCode.NormalizeLanguageCode(),
+                        "translate"
+                    ],
+                    Id = $"{modelPrefix}{languageCode}"
+                        .ToModelId(GetIdentifier()),
+                    Name = $"{displayName} Translate to {language.Name}",
+                    Description = language.NativeName
+                };
+            });
     }
 }
 
