dfd5d38d4c13bb851770842aeb6bc1dd5d0e56a3
Arthur Bleij <github@achappey.nl>
2026-08-21 20:26:13 +0200

    Mark Exa Agent models and reject unsupported video operations

--- Core/AIHappey.Core/Providers/Parallel/ParallelProvider.cs
+++ Core/AIHappey.Core/Providers/Parallel/ParallelProvider.cs
@@ -215,11 +215,11 @@ public partial class ParallelProvider : IModelProvider, IUnifiedModelProvider
 
     public Task<VideoOperationStartResult> StartVideoOperation(VideoRequest request, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 
     public Task<VideoOperationStatusResult> GetVideoOperationStatus(string operation, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 }
